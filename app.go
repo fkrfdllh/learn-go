ad835d2fd9e84914f15908ff9f3614382aeaa221
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-04 22:05:42 +0700

    constant variable

--- app.go
+++ app.go
@@ -15,11 +15,14 @@ import (
 
 func main() {
 	// investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0
+	const inflationRate float64 = 6.5
 	investmentAmount := 1000.0
 	expectedReturnRate := 5.5
 	years := 10.0
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
