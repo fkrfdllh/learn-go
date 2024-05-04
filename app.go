3920693a09ffdfaf7a8facc736ba29a69086dd73
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-04 10:38:29 +0700

    convert integer to float64 using float64()

--- app.go
+++ app.go
@@ -7,5 +7,5 @@ func main() {
 	var expectedReturnRate = 5.5 
 	var years = 10
 
-	var futureValue = investmentAmount * (1 + expectedReturnRate / 100)
+	var futureValue = float64(investmentAmount) * (1 + expectedReturnRate / 100)
 }
