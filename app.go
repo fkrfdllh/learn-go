0f532e37eb8850b84b010fb4790e732dedf413a4
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-06 10:48:02 +0700

    use returned values from function

--- app.go
+++ app.go
@@ -30,8 +30,9 @@ func main() {
 	outputText("Years: ")
 	fmt.Scanln(&years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	// futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	// futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
 
 	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
 	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.2f\n", futureRealValue)
