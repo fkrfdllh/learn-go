3ed91511531230555a1db5c8354cb4e75cda0c79
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-06 11:10:43 +0700

    initial code

--- app.go
+++ app.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world!")
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scanln(&revenue)
+
+	fmt.Print("Expenses: ")
+	fmt.Scanln(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scanln(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate / 100)
+	ratio := ebt / profit
+
+	fmt.Println(ebt)
+	fmt.Println(profit)
+	fmt.Println(ratio)
 }
