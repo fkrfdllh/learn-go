ee30714de161c01a913a7d95a0a83e103be9b222
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-06 13:05:57 +0700

    initialize workspace

--- app.go
+++ app.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world!")
+	fmt.Println("Welcome to GoBank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice uint8
+	fmt.Print("Your choice: ")
+	fmt.Scanln(&choice)
+
+	fmt.Println("Your choice is ", choice)
 }
