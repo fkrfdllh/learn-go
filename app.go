cfca6d6cadca216d1c565e10e3490f87f01328d5
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-08 09:00:10 +0700

    using if

--- app.go
+++ app.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance float64 = 1000
+
 	fmt.Println("Welcome to GoBank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
@@ -13,6 +15,10 @@ func main() {
 	var choice uint8
 	fmt.Print("Your choice: ")
 	fmt.Scanln(&choice)
+
+	if choice == 1 {
+		fmt.Printf("Your account balance is $%.2f\n", accountBalance)
+	}
 
 	fmt.Println("Your choice is ", choice)
 }
