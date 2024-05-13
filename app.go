8bb2f6919245b75a6f634e562fcb0a1773bd125b
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-13 14:22:12 +0700

    using switch instead of if/else

--- app.go
+++ app.go
@@ -22,47 +22,47 @@ func main() {
 		var choice uint8
 		fmt.Print("Your choice: ")
 		fmt.Scanln(&choice)
-	
-		if choice == 1 {
-			fmt.Printf("Your account balance is $%.2f\n", accountBalance)
-		} else if choice == 2 {
-			var depositAmount float64
+
+		switch choice {
+			case 1:
+				fmt.Printf("Your account balance is $%.2f\n", accountBalance)
+			case 2:
+				var depositAmount float64
 			
-			fmt.Print("Your deposit: $")
-			fmt.Scanln(&depositAmount)
+				fmt.Print("Your deposit: $")
+				fmt.Scanln(&depositAmount)
+		
+				if depositAmount <= 0 {
+					fmt.Println("Invalid amount. Deposit amount must be greater than 0.")
+					continue
+				}
+		
+				accountBalance += depositAmount
+		
+				fmt.Printf("Your updated account balance is $%.2f\n", accountBalance)
+			case 3:
+				var withdrawAmount float64
 	
-			if depositAmount <= 0 {
-				fmt.Println("Invalid amount. Deposit amount must be greater than 0.")
-				continue
-			}
-	
-			accountBalance += depositAmount
-	
-			fmt.Printf("Your updated account balance is $%.2f\n", accountBalance)
-		} else if choice == 3 {
-			var withdrawAmount float64
-	
-			fmt.Print("Withdrawal amount: $")
-			fmt.Scanln(&withdrawAmount)
-	
-			if withdrawAmount <= 0 {
-				fmt.Println("Invalid amount. Withdrawal amount must be greater than 0.")
-				continue
-			}
-	
-			if withdrawAmount > accountBalance {
-				fmt.Println("Invalid amount. Withdrawal amount must less than your account balance.")
-				continue
-			}
-			
-			accountBalance -= withdrawAmount
-	
-			fmt.Printf("Your updated account balance is $%.2f\n", accountBalance)
-		} else {
-			fmt.Println("Goodbye!")
-			break
+				fmt.Print("Withdrawal amount: $")
+				fmt.Scanln(&withdrawAmount)
+		
+				if withdrawAmount <= 0 {
+					fmt.Println("Invalid amount. Withdrawal amount must be greater than 0.")
+					continue
+				}
+		
+				if withdrawAmount > accountBalance {
+					fmt.Println("Invalid amount. Withdrawal amount must less than your account balance.")
+					continue
+				}
+				
+				accountBalance -= withdrawAmount
+		
+				fmt.Printf("Your updated account balance is $%.2f\n", accountBalance)
+			default:
+				fmt.Println("Goodbye!")
+				fmt.Println("Thank you for choosing our bank")
+				return
 		}
 	}
-
-	fmt.Println("Thank you for choosing our bank")
 }
