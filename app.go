e5fc76f2e209c2fbaa4ecbc3ed2a66689a4a6b60
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-17 10:24:07 +0700

    split code into multiple file

--- app.go
+++ app.go
@@ -49,13 +49,9 @@ func main() {
 
 			panic("Can't continue, sorry")
 		}
-		
-		fmt.Println("What do you want to do?")
-		fmt.Println("1. Check balance")
-		fmt.Println("2. Deposit money")
-		fmt.Println("3. Withdraw money")
-		fmt.Println("4. Exit")
 	
+		showOptions()
+		
 		var choice uint8
 		fmt.Print("Your choice: ")
 		fmt.Scanln(&choice)
