0377423bbacf1a3d4feee6999e77b50d955819ca
Fikri Fadillah <fkrfdllh@gmail.com>
2024-05-29 10:22:26 +0700

    initialize project

--- app.go
+++ app.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello world!")
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthDate := getUserData("Please enter your birth date (YYYY-MM-DD): ")
+
+	// TODO
+
+	fmt.Println(firstName, lastName, birthDate)
+}
+
+func getUserData(promptText string) string {
+	var value string	
+	
+	fmt.Print(promptText)
+	fmt.Scanln(&value)
+	
+	return value
 }
