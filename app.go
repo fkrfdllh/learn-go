818ea8b1d31eee816a111168502c23dcb7024d78
Fikri Fadillah <fkrfdllh@gmail.com>
2024-10-11 16:52:57 +0700

    refactor: finish interface preps

--- app.go
+++ app.go
@@ -4,12 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"learn-go/note"
+	"learn-go/todo"
 	"os"
 	"strings"
 )
 
 func main() {
 	title, content := getNoteData()
+	todoText := getUserInput("Todo text: ")
+
+	todo, err := todo.New(todoText)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	todo.Display()
+	err = todo.Save()
+
+	if err != nil {
+		fmt.Println("failed to save todo")
+
+		return
+	}
+
+	fmt.Println("todo saved")
 
 	userNote, err := note.New(title, content)
 
